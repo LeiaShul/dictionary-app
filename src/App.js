079fdac014d3affcad2299a008d8ba30edf9f438
LeiaShul <lesia.shulezhko24@gmail.com>
2022-12-16 15:59:12 +0200

    search engine and API integration

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
-        <Dictionary />
+        <Dictionary defaultKeyword="flower"/>
       </div>
       <footer className="text-center text-muted font-monospace" >
         This project was coded by Lesia Shulezhko and is <a className="link" href="https://github.com/LeiaShul/dictionary-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,68 @@
-import React from "react";
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-export default function Dictionary() {
-    return (
-        <div className="text-center">
-            <h1>Dictionary</h1>
-        </div>
-    )
+import Result from "./Result";
+
+export default function Dictionary(props) {
+    const [keyword, setKeyword] = useState(props.defaultKeyword);
+    const [definition, setDefinition] = useState(null);
+    const [loaded, setLoaded] = useState(false);
+
+    function search() {
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);        
+    }
+    function load() {
+        setLoaded(true);
+        search();
+    }
+    
+    function handleResponse(response) {
+      //  console.log(response.data[0]);
+        setDefinition(response.data[0]);      
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
+    }
+    function handleKeywordChange(event) {
+        event.preventDefault();
+        setKeyword(event.target.value);
+    }
+
+    if (loaded) {
+        return (
+            <div className="dictionary">
+                <header className="text-center">
+                    <h1 className="display-3">Dictionary</h1>
+                </header>
+                <div className="container search">
+                    <form onSubmit={handleSubmit}>
+                        <label className="m-3 display-5 fs-2">What word do you want to look up?</label>
+                        <div className="row">
+                            <div className="col-11">
+                                <input onChange={handleKeywordChange} className="form-control mx-2" type="search"></input>
+                            </div>
+                            <div className="col">
+                                <button type="submit" className="btn btn-outline-light mx-0">
+                                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                                </button>
+                            </div>
+                        </div>
+                        <div className="example ms-3 text-muted">
+                            i.e. paris, wine, yoga, coding
+                        </div>                        
+                    </form>
+                </div> 
+                <Result definition={definition} />
+            </div>
+        )
+    }
+    else {
+        load();
+        return "loading";
+    }
 }
