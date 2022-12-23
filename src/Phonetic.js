ea0833f2db2e5cb5284b5bcd71b18da1429792f0
LeiaShul <lesia.shulezhko24@gmail.com>
2022-12-23 16:45:23 +0100

    responsive phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,8 +5,8 @@ export default function Phonetic(props) {
     if (props.phonetic) {
         return (
             <div className="phonetic">
-                <div className="text-muted fs-3 row">
-                    <ReactAudioPlayer src={props.phonetic.audio} controls className="col-8 col-md-5 pb-2" />
+                <div className="text-muted fs-3 text-center text-sm-start row flex-wrap-reverse">
+                    <ReactAudioPlayer src={props.phonetic.audio} controls className="col-sm-8 pb-2" />
                     <span className="col p-0">{props.phonetic.text} </span>
                 </div>
                 
